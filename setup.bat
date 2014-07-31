a2caa5da9587afb48227e2606e99e10515c29333
Tamaki Mizuha <mizyoukan@outlook.com>
2014-07-31 12:55:34 +0900

    Remove Windows XP support

--- setup.bat
+++ setup.bat
@@ -1,27 +1,9 @@
 @echo off
+setlocal
 
-if not exist "%HOME%" (
-    echo please set environment variable %^HOME%.
-    goto finally
-)
+if "%HOME%"=="" set HOME=%USERPROFILE%
 
-ver|find "XP">nul
-if %errorlevel% == 0 goto make_hardlink
-goto make_symlink
+mklink "%HOME%\_vimrc" "%~dp0.vimrc"
+mklink "%HOME%\_gvimrc" "%~dp0.gvimrc"
 
-:make_symlink
-if exist "%HOME%\\_vimrc" del "%HOME%\\_vimrc"
-mklink "%HOME%\\_vimrc" "%~dp0.vimrc"
-if exist "%HOME%\\_gvimrc" del "%HOME%\\_gvimrc"
-mklink "%HOME%\\_gvimrc" "%~dp0.gvimrc"
-goto finally
-
-:make_hardlink
-if exist "%HOME%\\_vimrc" del "%HOME%\\_vimrc"
-fsutil hardlink create "%HOME%\\_vimrc" "%~dp0.vimrc"
-if exist "%HOME%\\_gvimrc" del "%HOME%\\_gvimrc"
-fsutil hardlink create "%HOME%\\_gvimrc" "%~dp0.gvimrc"
-goto finally
-
-:finally
 pause
